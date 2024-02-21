9d010e3a2baafd0a3dc0f833a19a5fe19abde9f4
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-02-21 16:35:35 +0100

    Working on favicon

--- pages/index.php
+++ pages/index.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+        include('../components/favicon.php');
+    ?>
     <title>Our Library - Home</title>
 </head>
 <body>
