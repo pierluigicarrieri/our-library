34b70acfdef4b97736a56018001b7204f01e96a7
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-02-26 18:35:06 +0100

    Added links to all pages in homepage and back

--- pages/index.php
+++ pages/index.php
@@ -8,6 +8,14 @@
 
     <main>
         <h1>Homepage</h1>
+        <div>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/login.php" class="btn btn-danger">Login</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/login_result.php" class="btn btn-danger">Login Results</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/logout_result.php" class="btn btn-danger">Logout Results</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/insert_book.php" class="btn btn-danger">Add New Book</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/book_insert_result.php" class="btn btn-danger">New Book Added</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/user_library.php" class="btn btn-danger">User Library</a>
+        </div>
     </main>
 
     <?php
---
+++ pages/insert_book.php
@@ -0,0 +1,21 @@
+<?php
+    $title = 'Our Library - Add New Book';
+?>
+
+    <?php
+        include('../components/header.php');
+    ?>
+
+    <main>
+        <h1>Add New Book</h1>
+        <div>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/index.php" class="btn btn-danger">Homepage</a>
+        </div>
+    </main>
+
+    <?php
+        include('../components/footer.php');
+    ?>
+
+</body>
+</html>
