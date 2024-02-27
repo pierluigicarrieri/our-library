f3d8560bae5be6119f559f60ee24b4f42a3309f8
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-02-27 16:25:25 +0100

    Working on cards

--- pages/index.php
+++ pages/index.php
@@ -11,15 +11,17 @@
     // Saves query in a variable, query gets 3 random books
     $query = 'SELECT * FROM Books
     ORDER BY RAND()
-    LIMIT 3';
-
+    LIMIT 4';
+    // Constructs a statement object with the query to use for the processing
     $statement = $db->prepare($query);
-
+    // Runs the query
     $statement->execute();
-
+    // Returns an instance of the result object later used to get the data
     $results = $statement->get_result();
+    // Returns the results of the query as an array of arrays (column name as inner arrays keys)
+    $results = $results->fetch_all(MYSQLI_ASSOC);
 
-    $results = $results->fetch_all();
+    var_dump($results);
 
 ?>
 
@@ -29,6 +31,26 @@
 
     <main>
         <h1 class="text-center text-light">Homepage</h1>
+        <div class="container">
+            <div class="row row-cols-4">
+
+                <?php foreach ($results as $book): ?>
+
+                <div class="col text-light">
+                    <div class="card" style="width: 18rem;">
+                        <img src= <?php echo $book['cover_img'] ?> class="card-img-top" alt="book_img">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <?php endforeach; ?>
+
+            </div>
+        </div>
     </main>
 
     <?php
