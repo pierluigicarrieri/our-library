42bd4dab9a8688dadef0b340063eb4fd1e4c226c
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-02-27 12:36:55 +0100

    Added jumbo in main, navigation links in footer

--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,17 @@
 <footer>
-    <div class="footer-top-wrapper d-flex justify-content-center bg-dark-subtle">
+    <div class="footer-top-wrapper d-flex flex-column align-items-center py-3 bg-dark-subtle">
         <div class="footer-top d-flex justify-content-between w-75 px-5 py-3">
             <div class="px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veritatis ut sapiente exercitationem rem! Ullam, dignissimos. Numquam facilis adipisci perferendis tenetur debitis asperiores ipsa optio, quia modi eaque rerum dolor!</div>
             <div class="px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus corrupti tempora cum natus sapiente quas repudiandae minus fugiat ipsum dicta dolorum, cupiditate nulla, fugit quod, dolore eaque aut eius?</div>
             <div class="px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus corrupti tempora cum natus sapiente quas repudiandae minus fugiat ipsum dicta dolorum, cupiditate nulla, fugit quod, dolore eaque aut eius?</div>
+        </div>
+        <div class="text-center py-3">
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/login.php" class="btn btn-danger">Login</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/login_result.php" class="btn btn-danger">Login Results</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/logout_result.php" class="btn btn-danger">Logout Results</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/insert_book.php" class="btn btn-danger">Add New Book</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/book_insert_result.php" class="btn btn-danger">New Book Added</a>
+            <a href="http://localhost:8888/Personal_Projects/our_library/pages/user_library.php" class="btn btn-danger">User Library</a>
         </div>
     </div>
     <div class="footer-bottom text-center p-2 text-light bg-secondary">Copyright 2024 | Pier & Bubu</div>
