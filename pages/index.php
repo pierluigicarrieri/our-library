177e3c1a6c51cef1967f309672441d976f8b6d51
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-02-27 15:40:30 +0100

    Done query

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,24 @@
     $title = 'Our Library - Home';
     // Variable for css sheet link
     $stylesheet = '../style/homepage.css';
+
     // Query for random books list
+
+    // Creates handle to database
+    $db = new mysqli('localhost', 'root', 'root', 'our_library');
+    // Saves query in a variable, query gets 3 random books
+    $query = 'SELECT * FROM Books
+    ORDER BY RAND()
+    LIMIT 3';
+
+    $statement = $db->prepare($query);
+
+    $statement->execute();
+
+    $results = $statement->get_result();
+
+    $results = $results->fetch_all();
+
 ?>
 
     <?php
