0921cf6fd2b9d97bfc7f56b8bb72f218d7f26bc1
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-03-05 18:17:21 +0100

    Added pics in footer, added super romantic phrase

--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,13 @@
 <footer>
     <div class="footer_top_wrapper d-flex flex-column align-items-center py-3">
         <div class="footer-top d-flex justify-content-between w-75 px-5 py-3">
-            <div class="px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veritatis ut sapiente exercitationem rem! Ullam, dignissimos. Numquam facilis adipisci perferendis tenetur debitis asperiores ipsa optio, quia modi eaque rerum dolor!</div>
-            <div class="px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus corrupti tempora cum natus sapiente quas repudiandae minus fugiat ipsum dicta dolorum, cupiditate nulla, fugit quod, dolore eaque aut eius?</div>
-            <div class="px-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus corrupti tempora cum natus sapiente quas repudiandae minus fugiat ipsum dicta dolorum, cupiditate nulla, fugit quod, dolore eaque aut eius?</div>
+            <div class="px-5">
+                <img src="../assets/site_pics/Pier.png" alt="Pier">
+            </div>
+            <div class="px-5 text-center">Now, my love, do you understand what you have done for me? <br/>And is it not a fearful thought, that a few slight circumstances might have prevented us from meeting?</div>
+            <div class="px-5">
+                <img src="../assets/site_pics/Bubu.png" alt="Bubu">
+            </div>
         </div>
         <div class="text-center py-3">
             <a href="http://localhost:8888/Personal_Projects/our_library/pages/login.php" class="btn btn-danger">Login</a>
