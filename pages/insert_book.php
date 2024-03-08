07a94cfade9d065ac74bf18a5a993ea3487adfd3
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-03-08 14:30:35 +0100

    Example

--- pages/insert_book.php
+++ pages/insert_book.php
@@ -1,7 +1,7 @@
 <?php
     // Starts session
     session_start();
-    
+
     $title = 'Our Library - Add New Book';
     // Variable for css sheet link
     $stylesheet = '../style/insert_book.css';
@@ -12,7 +12,7 @@
     ?>
 
     <main>
-        <h1>Add New Book</h1>
+        <h1>Add New Book and blabla</h1>
         <div>
             <a href="http://localhost:8888/Personal_Projects/our_library/pages/index.php" class="btn btn-danger">Homepage</a>
         </div>
