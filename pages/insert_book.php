09db2847cd75b1ac90d7f6df5251aad9660cae4e
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2024-03-11 17:20:56 +0100

    Basic form structure done

--- pages/insert_book.php
+++ pages/insert_book.php
@@ -20,16 +20,86 @@
                     <?php if(isset($_SESSION['logged_user'])) { ?>
 
                         <form action="book_insert_result.php" method="post" class="d-flex flex-column align-items-center m-auto text-light">
-                            <div class="mb-3">
-                                <label for="title" class="form-label">Title</label>
-                                <input type="text" class="form-control" name="title" id="title">
-                            </div>
-                            <div class="mb-3">
-                                <label for="ISBN" class="form-label">ISBN</label>
-                                <input type="text" class="form-control" name="ISBN">
-                            </div>
-                            <div class="button_wrapper d-flex justify-content-center">
-                                <button type="submit" class="btn btn-danger m-auto">Insert book</button>
+                            <div class="container">
+                                <div class="row row-cols-3">
+                                    <div class="col">
+                                        <label for="title" class="form-label">Title</label>
+                                        <input type="text" class="form-control" name="title" id="title">
+                                    </div>
+                                    <div class="col">
+                                        <label for="language" class="form-label">Language</label>
+                                        <input type="text" class="form-control" name="language" id="language">
+                                    </div>
+                                    <div class="col">
+                                        <label for="ISBN" class="form-label">ISBN</label>
+                                        <input type="text" class="form-control" name="ISBN" id="ISBN">
+                                    </div>
+                                </div>
+                                <div class="row row-cols-3">
+                                    <div class="col">
+                                        <label for="cover_img" class="form-label">Cover Image Link</label>
+                                        <input type="text" class="form-control" name="cover_img" id="cover_img">
+                                    </div>
+                                    <div class="col">
+                                        <label for="cover_type" class="form-label">Cover Type</label>
+                                        <input type="text" class="form-control" name="cover_type" id="cover_type">
+                                    </div>
+                                    <div class="col">
+                                        <label for="number_of_pages" class="form-label">Pages</label>
+                                        <input type="text" class="form-control" name="number_of_pages" id="number_of_pages">
+                                    </div>
+                                </div>
+                                <div class="row row-cols-2">
+                                    <div class="col">
+                                        <label for="publisher" class="form-label">Publisher</label>
+                                        <input type="text" class="form-control" name="publisher" id="publisher">
+                                    </div>
+                                    <div class="col">
+                                        <label for="publication_date" class="form-label">Publication Date</label>
+                                        <input type="date" class="form-control" name="publication_date" id="publication_date">
+                                    </div>
+                                </div>
+                                <div class="row row-cols-1">
+                                    <div class="col">
+                                        <label for="description" class="form-label">Description</label>
+                                        <textarea class="form-control" name="description" id="description" rows="4"></textarea>
+                                    </div>
+                                </div>
+                                <div class="row row-cols-3">
+                                    <div class="col">
+                                        <label for="author_1" class="form-label">Author 1</label>
+                                        <input type="text" class="form-control" name="author_1" id="author_1">
+                                    </div>
+                                    <div class="col">
+                                        <label for="author_2" class="form-label">Author 2</label>
+                                        <input type="text" class="form-control" name="author_2" id="author_2">
+                                    </div>
+                                    <div class="col">
+                                        <label for="author_3" class="form-label">Author 3</label>
+                                        <input type="text" class="form-control" name="author_3" id="author_3">
+                                    </div>
+                                </div>
+                                <div class="row row-cols-3">
+                                    <div class="col">
+                                        <label for="genre_1" class="form-label">Genre 1</label>
+                                        <input type="text" class="form-control" name="genre_1" id="genre_1">
+                                    </div>
+                                    <div class="col">
+                                        <label for="genre_2" class="form-label">Genre 2</label>
+                                        <input type="text" class="form-control" name="genre_2" id="genre_2">
+                                    </div>
+                                    <div class="col">
+                                        <label for="genre_3" class="form-label">Genre 3</label>
+                                        <input type="text" class="form-control" name="genre_3" id="genre_3">
+                                    </div>
+                                </div>
+                                <div class="row row-cols-1">
+                                    <div class="col">
+                                        <div class="button_wrapper d-flex justify-content-center">
+                                            <button type="submit" class="btn btn-danger m-auto">Insert book</button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </form>
                     <!-- If user isn't logged in shows error message -->
@@ -53,3 +123,18 @@
 
 </body>
 </html>
+
+
+
+
+<div class="mb-3">
+    <label for="title" class="form-label">Title</label>
+    <input type="text" class="form-control" name="title" id="title">
+</div>
+<div class="mb-3">
+    <label for="ISBN" class="form-label">ISBN</label>
+    <input type="text" class="form-control" name="ISBN">
+</div>
+<div class="button_wrapper d-flex justify-content-center">
+    <button type="submit" class="btn btn-danger m-auto">Insert book</button>
+</div>
